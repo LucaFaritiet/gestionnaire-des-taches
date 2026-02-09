6a8363ceeb2c8403a90f9114b8430e84e45d3327
Esteban <estebarillot@gmail.com>
2026-02-09 17:17:02 +0100

    feat: interface

--- Workshop5Avaloniapp/App.axaml.cs
+++ Workshop5Avaloniapp/App.axaml.cs
@@ -25,7 +25,7 @@ namespace Workshop5Avaloniapp
                 DisableAvaloniaDataAnnotationValidation();
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = new ListProcessViewModel(),
                 };
             }
 
