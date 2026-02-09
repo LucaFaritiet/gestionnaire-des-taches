5939ec521d84d051dfcfb4282ac9af8de976690a
Esteban <estebarillot@gmail.com>
2026-02-09 17:16:42 +0100

    feat: interface IHM

---
+++ Workshop5Avaloniapp/ViewModels/ListProcessViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+namespace Workshop5Avaloniapp.ViewModels;
+
+public class ListProcessViewModel
+{
+    public Process[] Processes { get; } =  Process.GetProcesses();
+}
---
+++ Workshop5Avaloniapp/Views/ListProcess.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+
+namespace Workshop5Avaloniapp.Views
+{
+    public partial class ListProcess : Window
+    {
+        public ListProcess()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
