6700a971b0d3c597f4ea3ca58931fd63d060d26a
faritietl <luca.faritiet@viacesi.fr>
2026-02-09 16:39:21 +0100

    add: project organisation

---
+++ Workshop5Avaloniapp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Workshop5Avaloniapp.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Sacré gestionnaire des tâches!";
+    }
+}
